921f33e84a41bcba10011a3deaddbcac346ba2e1
Arthur Victoriano <arthurvicto@hotmail.com>
2022-12-14 23:59:54 -0300

    test

--- src/Tryitter.Test/TestPostsController.cs
+++ src/Tryitter.Test/TestPostsController.cs
@@ -22,17 +22,19 @@ public class TestPostController
     {
     }
 
-    [Fact]
-    public async void PostsByStudents()
+    [Theory]
+    [InlineData(0)]
+    public async void PostsByStudents(int i)
     {
 
         var a = new Mock<IPostRepository>();
-        a.Setup(x => x.PostsByStudentId(0)).Returns(It.IsAny<List<Post>>);
+        a.Setup(x => x.PostsByStudentId(i)).Returns(It.IsAny<List<Post>>());
 
         var _controller = new PostController(a.Object);
-        var b =_controller.GetPostsByStudentId(0);
+        var b = _controller.GetPostsByStudentId(i);
 
-        a.Verify(x => x.PostsByStudentId(0), Times.Once);
+        a.Verify(x => x.PostsByStudentId(i), Times.Once);
+        b.Should().NotBeNull();
     }
 
     [Fact]
@@ -46,6 +48,7 @@ public class TestPostController
         var b = _controller.GetLastPostByStudentId(0);
 
         a.Verify(x => x.LastPostsByStudentId(0), Times.Once);
+        b.Should().NotBeNull();
     }
 
     [Fact]
@@ -56,9 +59,12 @@ public class TestPostController
         a.Setup(x => x.GetPostById(0)).Returns(It.IsAny<Post>);
 
         var _controller = new PostController(a.Object);
+        Post newPost = new() { PostId = 0, Messages = "aa", Image = "aa", StudentId = 0 };
+        _controller.Post(newPost);
         var b = _controller.Get(0);
 
         a.Verify(x => x.GetPostById(0), Times.Once);
+        b.Should().NotBeNull();
     }
 
     [Fact]
@@ -69,10 +75,11 @@ public class TestPostController
         a.Setup(x => x.PostMessage(It.IsAny<Post>())).Returns(It.IsAny<Post>());
 
         var _controller = new PostController(a.Object);
-        Post newPost = new() {PostId = 0, Messages = "'aa'", Image = "'aa'", StudentId = 0 };
+        Post newPost = new() { PostId = 0, Messages = "aa", Image = "aa", StudentId = 0 };
         var b = _controller.Post(newPost);
 
         a.Verify(x => x.PostMessage(It.IsAny<Post>()), Times.Once);
+        b.Should().NotBeNull();
     }
     [Fact]
     public async void PutPosts()
@@ -82,16 +89,31 @@ public class TestPostController
         a.Setup(x => x.PutMessage(It.IsAny<Post>())).Returns(It.IsAny<Post>());
 
         var _controller = new PostController(a.Object);
-        Post newPost = new() { PostId = 0, Messages = "'aa'", Image = "'aa'", StudentId = 0 };
+        Post newPost = new() { PostId = 0, Messages = "aa", Image = "aa", StudentId = 0 };
         var b = _controller.Put(0, newPost);
 
         a.Verify(x => x.PutMessage(It.IsAny<Post>()), Times.Once);
+        b.Should().NotBeNull();
+    }
+
+    [Fact]
+    public async void PutPosts_ShouldReturnNull()
+    {
+
+        var a = new Mock<IPostRepository>();
+        a.Setup(x => x.PutMessage(It.IsAny<Post>())).Returns(It.IsAny<Post>());
+
+        var _controller = new PostController(a.Object);
+        Post newPost = new() { PostId = 0, Messages = "aa", Image = "aa", StudentId = 0 };
+        var b = _controller.Put(1, newPost);
+
+        a.Verify(x => x.PutMessage(It.IsAny<Post>()), Times.Never);
     }
 
     [Fact]
     public async void DeletePosts()
     {
-        Post newPost = new() { PostId = 0, Messages = "'aa'", Image = "'aa'", StudentId = 0 };
+        Post newPost = new() { PostId = 0, Messages = "aa", Image = "aa", StudentId = 0 };
 
         var a = new Mock<IPostRepository>();
         a.Setup(x => x.DeleteMessage(It.IsAny<Post>())).Returns(It.IsAny<Post>());
@@ -101,5 +123,22 @@ public class TestPostController
         var b = _controller.Delete(0);
 
         a.Verify(x => x.DeleteMessage(It.IsAny<Post>()), Times.Once);
+        b.Should().NotBeNull();
+    }
+
+    [Fact]
+    public async void DeletePosts_ShouldReturnNull()
+    {
+        Post newPost = new() { PostId = 0, Messages = "aa", Image = "aa", StudentId = 0 };
+
+        var a = new Mock<IPostRepository>();
+        a.Setup(x => x.GetPostById(0)).Returns(newPost);
+        a.Setup(x => x.DeleteMessage(It.IsAny<Post>())).Returns(It.IsAny<Post>());
+
+        var _controller = new PostController(a.Object);
+        var b = _controller.Delete(1);
+
+        a.Verify(x => x.DeleteMessage(It.IsAny<Post>()), Times.Never);
     }
 }
+
